4854242e5f7942aacbb90849f12460fcf7175d1d
gince <2727904+gince@users.noreply.github.com>
2024-11-13 14:50:48 -0500

    in progress. right before working with db.

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -4,7 +4,7 @@ import clsx from 'clsx';
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
-      className={clsx(
+      className={clsx( /* clsx allows you to toggle between different styles based on a condition (status here) */
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
           'bg-gray-100 text-gray-500': status === 'pending',
