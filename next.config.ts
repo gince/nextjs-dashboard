3e2e45530e24ea0aabb3f6deabe6efacab4dc2d7
gince <2727904+gince@users.noreply.github.com>
2024-11-14 09:53:40 -0500

    experimental ppr disabled.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    ppr: 'incremental', // partial pre-rendering
-  },
+  // experimental: {
+  //   ppr: 'incremental', // partial pre-rendering
+  // },
 };
 
 export default nextConfig;
